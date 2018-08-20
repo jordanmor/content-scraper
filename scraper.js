15521c3f41bc2d89343f3fb33b7e7c9795afb07d
Jordan Mor <jordanmor@gmail.com>
2018-08-20 17:53:57 -0400

    Extract urls that point to shirt data from main products page url

--- scraper.js
+++ scraper.js
@@ -24,6 +24,8 @@ fs.mkdir('data', err => err);
 
 fetch('http://shirts4mike.com/shirts.php')
   .then(checkStatus)
+  .then(scrapeUrls)
+  .then(urls => console.log(urls))
   .catch(error => console.log(error.message));
 
 function checkStatus(response) {
@@ -32,4 +34,19 @@ function checkStatus(response) {
   } else {
     throw new Error(`Connection error: There's been a ${response.status} error. Cannot connect to ${response.url}.`);
   }
+}
+
+// Extract urls that point to shirt data from main products page url using jsdom and jquery
+function scrapeUrls(html) {
+  const dom = new JSDOM(html);
+  const $ = (require('jquery'))(dom.window);
+
+  /* Create a jQuery collection of all relevant links to shirt info by selecting every <a> element 
+     whose href attribute value begins with "shirt.php?id=". Map through this colleciton to create 
+     the URLs where the shirt data can be accessed and scraped. */
+  const urls = $('a[href^="shirt.php?id="]').map(function() {
+    return `http://shirts4mike.com/${this.getAttribute('href')}`;
+  });
+  
+  return $.makeArray(urls); // return a regular array
 }
