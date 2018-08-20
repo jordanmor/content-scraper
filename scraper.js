b153a03a78a9357f0611b2da448b2c0f550f1b5f
Jordan Mor <jordanmor@gmail.com>
2018-08-20 14:50:55 -0400

    Add jsdom as project dependency

--- scraper.js
+++ scraper.js
@@ -9,6 +9,15 @@ const fs = require('fs');
  */
 const fetch = require('node-fetch');
 
+/* 
+ * Reasons for using jsdom
+ * 1. Over 2 million downloads a week on npm & update last published within the last week.
+ * 2. Chose over Cheerio because Cheerio has not been updated for a year
+ */
+const jsdom = require('jsdom');
+
+const { JSDOM } = jsdom;
+
 /* Data folder created if one does not exist. If it does exist, 
    an unlogged error is thrown, which does not affect the rest of the program. */
 fs.mkdir('data', err => err);
@@ -19,7 +28,7 @@ fetch('http://shirts4mike.com/shirts.php')
 
 function checkStatus(response) {
   if (response.ok) {
-    return response.text()
+    return response.text();
   } else {
     throw new Error(`Connection error: There's been a ${response.status} error. Cannot connect to ${response.url}.`);
   }
