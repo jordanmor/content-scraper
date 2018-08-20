6e79d7596e4ba880096adbc0d818f1dbf7806921
Jordan Mor <jordanmor@gmail.com>
2018-08-20 10:35:41 -0400

    Program creates a data folder if one does not already exist

--- scraper.js
+++ scraper.js
@@ -1,1 +1,7 @@
-"use strict"
+"use strict";
+
+const fs = require('fs');
+
+/* Data folder created if one does not exist. If it does exist, 
+   an unlogged error is thrown, which does not affect the rest of the program. */
+fs.mkdir('data', err => err);
