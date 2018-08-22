237a3d18675a3af04fccafc67321a6e6e337d7e2
Jordan Mor <jordanmor@gmail.com>
2018-08-22 07:53:51 -0400

    Scrape required information

--- scraper.js
+++ scraper.js
@@ -19,14 +19,24 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 /* Data folder created if one does not exist. If it does exist, 
-   an unlogged error is thrown, which does not affect the rest of the program. */
+an unlogged error is thrown, which does not affect the rest of the program. */
+
 fs.mkdir('data', err => err);
+
+/*=============-=============-=============-=============
+                        FETCH DATA
+===============-=============-=============-===========*/
 
 fetch('http://shirts4mike.com/shirts.php')
   .then(checkStatus)
   .then(scrapeUrls)
-  .then(urls => console.log(urls))
+  .then(scrapeData)
+  .then(data => console.log(data))
   .catch(error => console.log(error.message));
+
+/*=============-=============-=============-=============
+                        FUNCTIONS
+===============-=============-=============-===========*/
 
 function checkStatus(response) {
   if (response.ok) {
@@ -42,11 +52,44 @@ function scrapeUrls(html) {
   const $ = (require('jquery'))(dom.window);
 
   /* Create a jQuery collection of all relevant links to shirt info by selecting every <a> element 
-     whose href attribute value begins with "shirt.php?id=". Map through this colleciton to create 
-     the URLs where the shirt data can be accessed and scraped. */
+  whose href attribute value begins with "shirt.php?id=". Map through this colleciton to create 
+  the URLs where the shirt data can be accessed and scraped. */
+
   const urls = $('a[href^="shirt.php?id="]').map(function() {
     return `http://shirts4mike.com/${this.getAttribute('href')}`;
   });
   
   return $.makeArray(urls); // return a regular array
+}
+
+/* Pass an array of promises to Promise.all() to return a single promise 
+that's resolved when all the data promises have resolved. 
+Make array iteration easier using async/await */
+
+async function scrapeData(urls) {
+  const extractedDataArray = urls.map(async url => {
+      const data = await fetch(url)
+          .then(checkStatus)
+          .then(extractData)
+          .catch(error => console.log(error.message));
+      data.URL = url;
+      return data;
+  });
+  return await Promise.all(extractedDataArray);
+}
+
+/* Use jsdom and jquery to scrape data from each shirt product page and return
+relevant information in an object that can be converted to a csv file */
+
+function extractData(html) {
+  const dom = new JSDOM(html);
+  const $ = (require('jquery'))(dom.window);
+  const $shirtImg = $('.shirt-picture img');
+  return {
+      title: $shirtImg.attr('alt'),
+      price: $('.shirt-details .price').text(),
+      imageURL: `http://shirts4mike.com/${$shirtImg.attr('src')}`,
+      URL: '',
+      time: new Date().toISOString().slice(11,19)
+  }
 }
